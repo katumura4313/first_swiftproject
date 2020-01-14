368753d3d22be96f8fec06088c913a74bb6d1d58
katsumura4313 <n4ru4313@gmail.com>
2020-01-14 20:17:46 +0900

    セットアップ完了

---
+++ first_swiftproject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  first_swiftproject
+//
+//  Created by USER on 2020/01/14.
+//  Copyright © 2020 jp.go. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
