e8893fe22474ef88ec30216fa5d8b65fa02dea31
katumura4313 <n4ru4313@gmail.com>
2020-01-21 19:24:18 +0900

    tableviewcontrolle作成

---
+++ first_swiftproject/ViewControllers/TableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TableViewController.swift
+//  first_swiftproject
+//
+//  Created by USER on 2020/01/21.
+//  Copyright © 2020 jp.go. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
